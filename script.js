f45c6db56a73774fd577025f95e9d20d2073fc41
Hashmat-Noorani <hashmatwani@icloud.com>
2023-01-06 01:06:57 +0530

    Added: minimax algorithm

--- script.js
+++ script.js
@@ -1,188 +1,322 @@
 //selecting all required elements
 const selectBox = document.querySelector(".select-box"),
-  selectBtnX = selectBox.querySelector(".options .playerX"),
-  selectBtnO = selectBox.querySelector(".options .playerO"),
+  startBtn = document.querySelector(".start"),
+  coming = document.querySelector(".coming"),
   singlePlay = selectBox.querySelector(".options .singlePlay"),
   multiPlay = selectBox.querySelector(".options .multiPlay"),
+  onlinePlay = selectBox.querySelector(".options .onlinePlay"),
+  singlePlayDashboard = selectBox.querySelector(
+    ".options .singlePlayDashboard"
+  ),
+  helperText = selectBox.querySelector("small.helperText"),
+  selectLevel = selectBox.querySelector(".options .level"),
+  selectPlayer = selectBox.querySelector(".options .player"),
+  selectBoardSize = selectBox.querySelector(".options .board"),
   playBoard = document.querySelector(".play-board"),
-  players = document.querySelector(".players"),
-  allBox = document.querySelectorAll("section span"),
+  xTurn = playBoard.querySelector(".xTurn"),
+  oTurn = playBoard.querySelector(".oTurn"),
+  cells = document.querySelectorAll("section span"),
   resultBox = document.querySelector(".result-box"),
   wonText = resultBox.querySelector(".won-text"),
-  replayBtn = resultBox.querySelector("button");
+  replayBtn = resultBox.querySelector("button"),
+  close = document.querySelector(".material-icons.close");
 
-let playerXIcon = "fas fa-times"; //class name of fontawesome cross icon
-let playerOIcon = "far fa-circle"; //class name of fontawesome circle icon
-let playerSign = "X"; //this is a global variable beacuse we've used this variable inside multiple functions
-let runBot = true; //this also a global variable with boolen value..we used this variable to stop the bot once match won by someone or drawn
-let selectedPlayer = "X";
-let multiPlayer = false;
+let xTurnSound = new Audio("./files/xTurn.wav");
+let oTurnSound = new Audio("./files/oTurn.wav");
+let gameStartSound = new Audio("./files/gameStart.wav");
+let gameWinSound = new Audio("./files/gameWin.wav");
+let gameLoseSound = new Audio("./files/gameLose.wav");
+let gameTieSound = new Audio("./files/gameTie.wav");
+
+let orgBoard;
+let boardSize = 3;
+let gameMode = "single";
+let level = "easy";
+let human = "X";
+let ai = "O";
+let tie = null;
+let turn;
+let volume = JSON.parse(localStorage.getItem("volume")) ?? true;
+
+let winCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2],
+];
 
 window.onload = () => {
-  //once window loaded
-  for (let i = 0; i < allBox.length; i++) {
-    //add onclick attribute in all available span
-    allBox[i].setAttribute("onclick", "clickedBox(this)");
-  }
+  homeSound.classList.add(volume ? "fa-volume-high" : "fa-volume-xmark");
 };
 
-singlePlay.addEventListener("click", () => {
-  runBot = true;
+startBtn.addEventListener("click", startGame);
+
+singlePlay.onclick = () => {
   singlePlay.classList.add("active");
   multiPlay.classList.remove("active");
-  multiPlayer = false;
-});
-
-multiPlay.addEventListener("click", () => {
-  runBot = false;
+  onlinePlay.classList.remove("active");
+  singlePlayDashboard.classList.add("active");
+  helperText.classList.add("active");
+  gameMode = "single";
+};
+multiPlay.onclick = () => {
   multiPlay.classList.add("active");
   singlePlay.classList.remove("active");
-  multiPlayer = true;
+  onlinePlay.classList.remove("active");
+  singlePlayDashboard.classList.remove("active");
+  helperText.classList.remove("active");
+  gameMode = "multi";
+  turn = "X";
+};
+onlinePlay.onclick = () => {
+  coming.classList.add("show");
+  selectBox.style.pointerEvents = "none";
+  onlinePlay.classList.add("active");
+  multiPlay.classList.remove("active");
+  singlePlay.classList.remove("active");
+  singlePlayDashboard.classList.remove("active");
+  helperText.classList.remove("active");
+  gameMode = "online";
+};
+
+close.onclick = reload;
+document.querySelector(".quit").onclick = reload;
+
+selectLevel.addEventListener("change", (e) => {
+  level = e.target.value;
+  console.log(level);
 });
 
-selectBtnX.onclick = () => {
-  selectBox.classList.add("hide"); //hide select box
-  playBoard.classList.add("show"); //show the playboard section
-  selectedPlayer = "X";
-};
+selectPlayer.addEventListener("change", (e) => {
+  human = e.target.value;
+  ai = human === "X" ? "O" : "X";
+});
 
-selectBtnO.onclick = () => {
-  selectBox.classList.add("hide"); //hide select box
-  playBoard.classList.add("show"); //show the playboard section
-  players.setAttribute("class", "players active player_O"); //set class attribute in players with players active player values
-  selectedPlayer = "O";
-};
+selectBoardSize.addEventListener("change", (e) => {
+  if (e.target.value == 5) {
+    coming.classList.add("show");
+    selectBox.style.pointerEvents = "none";
+  } else {
+    boardSize = e.target.value;
+  }
+});
+function startGame() {
+  playBoardSound.classList.add(volume ? "fa-volume-high" : "fa-volume-xmark");
+  volume && gameStartSound.play();
+  ox.classList.add("hide"); //hide select box
 
-// user click function
+  playBoard.classList.add("show"); //show the playboard section
+
+  for (let i = 0; i < cells.length; i++) {
+    orgBoard = Array.from(Array(9).keys());
+    //add onclick attribute in all available span
+    cells[i].addEventListener("click", clickedBox, false);
+  }
+  if (ai === "X") {
+    setTimeout(() => {
+      move(bestMove(), ai);
+    }, 0);
+  }
+}
+
 function clickedBox(element) {
-  if (players.classList.contains("player_O")) {
-    playerSign = "O"; //if player choose (O) then change playerSign to O
-    element.innerHTML = `<i class="${playerOIcon}"></i>`; //adding circle icon tag inside user clicked element/box
-    players.classList.remove("active"); //removing active class in players
-    element.setAttribute("id", playerSign); //set id attribute in span/box with player choosen sign
+  if (gameMode === "single") {
+    move(element.target.id, human);
   } else {
-    playerSign = "X";
-    element.innerHTML = `<i class="${playerXIcon}"></i>`; //adding cross icon tag inside user clicked element/box
-    element.setAttribute("id", playerSign); //set id attribute in span/box with player choosen sign
-    players.classList.add("active"); //add active class in players
+    move(element.target.id, turn);
+    turn = turn === "X" ? "O" : "X";
   }
-  element.style.pointerEvents = "none"; //once user select any box then that box can'be clicked again
-
-  if (runBot) {
-    //add pointerEvents none to playboard so user can't immediately click on any other box until bot select
+  if (gameMode === "single" && !tie && !checkWin(orgBoard, human)) {
     playBoard.style.pointerEvents = "none";
+    let randomTimeDelay = (Math.random() * 1000 + 200).toFixed(); //generating random number so bot will randomly delay to select unselected box
+    setTimeout(() => {
+      move(
+        level === "easy"
+          ? easyMove()
+          : level === "medium"
+          ? medMove()
+          : bestMove(),
+        ai
+      ); //calling bot function
+    }, randomTimeDelay); //passing random delay value
+  }
+}
+
+function move(id, player) {
+  gameStartSound.pause();
+  if (volume) player === "X" ? xTurnSound.play() : oTurnSound.play();
+  cells[id].style.pointerEvents = "none";
+  if (player === "X") {
+    xTurn.classList.remove("active");
+    oTurn.classList.add("active");
   } else {
-    // toggleplayer O to X and viceverse
-    players.classList.toggle("player_O");
+    oTurn.classList.remove("active");
+    xTurn.classList.add("active");
   }
-  if (selectWinner()) return; //calling selectWinner function
-  let randomTimeDelay = (Math.random() * 1000 + 200).toFixed(); //generating random number so bot will randomly delay to select unselected box
+  orgBoard[id] = player;
+  document.getElementById(id).innerText = player;
+  let winner = checkWin(orgBoard, player);
+  if (winner) {
+    gameOver(winner);
+    return;
+  }
+  checkTie();
+  console.log("reached");
+  playBoard.style.pointerEvents = "auto"; //add pointerEvents auto in playboard so user can again click on box
+}
+
+function checkWin(board, player) {
+  let winner = null;
+  let plays = board.reduce((a, e, i) => (e === player ? a.concat(i) : a), []);
+  for (let [i, win] of winCombos.entries()) {
+    if (win.every((el) => plays.indexOf(el) > -1)) {
+      winner = { i, player };
+      break;
+    }
+  }
+  return winner;
+}
+
+function gameOver(winner) {
+  console.log(winner);
+  for (let i of winCombos[winner.i]) {
+    document.getElementById(i).style.color =
+      gameMode === "multi"
+        ? "green"
+        : winner.player === human
+        ? "green"
+        : "tomato";
+  }
+  playBoard.style.pointerEvents = "none";
+  if (gameMode === "multi") {
+    declareWinner(`${winner.player} win!`);
+  } else {
+    declareWinner(
+      winner.player === human ? "Hurray, you win!" : "Sorry, you lose!"
+    );
+  }
+}
+
+function declareWinner(who) {
+  console.log(who);
+  wonText.innerHTML = who;
+
   setTimeout(() => {
-    bot(); //calling bot function
-  }, randomTimeDelay); //passing random delay value
+    if (volume)
+      who.includes("win")
+        ? gameWinSound.play()
+        : who.includes("lose")
+        ? gameLoseSound.play()
+        : gameTieSound.play();
+    //after match won by someone then show the result box after 700ms
+    resultBox.classList.add("show", who.includes("lose") && "lose");
+  }, 500);
+}
+function easyMove() {
+  return emptySpaces()[0];
+}
+function medMove() {
+  let availableSpots = emptySpaces();
+  return availableSpots[Math.floor(Math.random() * availableSpots.length)]; //getting random index from array so bot will select random unselected box
 }
 
-// bot auto select function
-function bot() {
-  let array = []; //creating empty array...we'll store unclicked boxes index
-  if (runBot) {
-    //if runBot is true
-    for (let i = 0; i < allBox.length; i++) {
-      if (allBox[i].childElementCount == 0) {
-        //if the box/span has no children means <i> tag
-        array.push(i); //inserting unclicked boxes number/index inside array
-      }
-    }
-    let randomBox = array[Math.floor(Math.random() * array.length)]; //getting random index from array so bot will select random unselected box
-    if (array.length > 0) {
-      //if array length is greater than 0
-      if (players.classList.contains("player_O")) {
-        playerSign = "X"; //if player has chosen O then bot will X
-        allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`; //adding cross icon tag inside bot selected element
-        allBox[randomBox].setAttribute("id", playerSign); //set id attribute in span/box with player choosen sign
-        players.classList.add("active"); //add active class in players
-      } else {
-        playerSign = "O"; //change the playerSign to O so if player has chosen X then bot will O
-        allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`; //adding circle icon tag inside bot selected element
-        players.classList.remove("active"); //remove active class in players
-        allBox[randomBox].setAttribute("id", playerSign); //set id attribute in span/box with player choosen sign
-      }
-      allBox[randomBox].style.pointerEvents = "none"; //once bot select any box then user can't click on that box
-      if (selectWinner()) return; //calling selectWinner function
+function bestMove() {
+  return minimax(orgBoard, ai).index;
+}
+function emptySpaces() {
+  return orgBoard.filter((s) => typeof s === "number");
+}
 
-      playBoard.style.pointerEvents = "auto"; //add pointerEvents auto in playboard so user can again click on box
+function checkTie() {
+  if (emptySpaces().length === 0) {
+    for (let i = 0; i < cells.length; i++) {
+      cells[i].style.color = "green";
     }
+    playBoard.style.pointerEvents = "none";
+    tie = true;
+    declareWinner("Tie Game");
   }
 }
 
-function getIdVal(classname) {
-  return document.querySelector(".box" + classname).id; //return id value
-}
-function checkIdSign(val1, val2, val3, sign) {
-  //checking all id value is equal to sign (X or O) or not if yes then return true
-  if (
-    getIdVal(val1) == sign &&
-    getIdVal(val2) == sign &&
-    getIdVal(val3) == sign
-  ) {
-    return true;
+function minimax(newBoard, player) {
+  let availableSpots = emptySpaces(newBoard);
+  if (checkWin(newBoard, human)) {
+    return { score: -10 };
+  } else if (checkWin(newBoard, ai)) {
+    return { score: 10 };
+  } else if (availableSpots.length === 0) {
+    return { score: 0 };
   }
-}
-function selectWinner() {
-  //if the one of following winning combination match then select the winner
-  if (
-    checkIdSign(1, 2, 3, playerSign) ||
-    checkIdSign(4, 5, 6, playerSign) ||
-    checkIdSign(7, 8, 9, playerSign) ||
-    checkIdSign(1, 4, 7, playerSign) ||
-    checkIdSign(2, 5, 8, playerSign) ||
-    checkIdSign(3, 6, 9, playerSign) ||
-    checkIdSign(1, 5, 9, playerSign) ||
-    checkIdSign(3, 5, 7, playerSign)
-  ) {
-    console.log(playerSign);
-    runBot = false; //passing the false boolen value to runBot so bot won't run again
-    // bot(); //calling bot function
-    setTimeout(() => {
-      //after match won by someone then hide the playboard and show the result box after 700ms
-      resultBox.classList.add("show");
-      playBoard.classList.remove("show");
-    }, 700); //1s = 1000ms
-    console.log(runBot);
-    if (multiPlayer) {
-      wonText.innerHTML = `Player <p>${playerSign}</p> won the game!`; //displaying winning text with passing playerSign (X or O)
-    } else if (playerSign === selectedPlayer) {
-      wonText.innerHTML = `Hurray, you won the game!`;
+  // an array to collect all the objects
+  let moves = [];
+  // loop through available spots
+  for (let i = 0; i < availableSpots.length; i++) {
+    //create an object for each and store the index of that spot
+    let move = {};
+    move.index = newBoard[availableSpots[i]];
+    // set the empty spot to the current player
+    newBoard[availableSpots[i]] = player;
+    /*collect the score resulted from calling minimax 
+      on the opponent of the current player*/
+    if (player === ai) {
+      let res = minimax(newBoard, human);
+      move.score = res.score;
     } else {
-      wonText.innerHTML = `Sorry, you lose the game!`;
+      let res = minimax(newBoard, ai);
+      move.score = res.score;
     }
-    return true;
-  } else if (
-    getIdVal(1) != "" &&
-    getIdVal(2) != "" &&
-    getIdVal(3) != "" &&
-    getIdVal(4) != "" &&
-    getIdVal(5) != "" &&
-    getIdVal(6) != "" &&
-    getIdVal(7) != "" &&
-    getIdVal(8) != "" &&
-    getIdVal(9) != ""
-  ) {
-    //if all boxes/element have id value and still no one win then draw the match
-
-    runBot = false; //passing the false boolen value to runBot so bot won't run again
-    // bot(); //calling bot function
-    setTimeout(() => {
-      //after match drawn then hide the playboard and show the result box after 700ms
-      resultBox.classList.add("show");
-      playBoard.classList.remove("show");
-    }, 700); //1s = 1000ms
-    wonText.textContent = "Match has been drawn!"; //displaying draw match text
-    return true;
+    // reset the spot to empty back to original
+    newBoard[availableSpots[i]] = move.index;
+    // push the object to the array
+    moves.push(move);
   }
-  return false;
+  // if it is the computer's turn loop over the moves and choose the move with the highest score
+  let bestMove;
+  if (player === ai) {
+    let bestScore = -Infinity;
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i].score > bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  } else {
+    // else loop over the moves and choose the move with the lowest score
+    let bestScore = Infinity;
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i].score < bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  }
+  // return the chosen move (object) from the moves array
+  return moves[bestMove];
 }
+replayBtn.onclick = reload;
 
-replayBtn.onclick = () => {
-  window.location.reload(); //reload the current page on replay button click
+function reload() {
+  window.location.reload();
+}
+let homeSound = document.querySelector(".homeSound");
+homeSound.onclick = () => {
+  homeSound.classList.add(volume ? "fa-volume-xmark" : "fa-volume-high");
+  homeSound.classList.remove(volume ? "fa-volume-high" : "fa-volume-xmark");
+  !volume && new Audio("./files/soundOnOff.wav").play();
+  localStorage.setItem("volume", !volume);
+  volume = !volume;
+};
+
+let playBoardSound = document.querySelector(".playBoardSound");
+playBoardSound.onclick = () => {
+  homeSound.classList.add(volume ? "fa-volume-xmark" : "fa-volume-high");
+  homeSound.classList.remove(volume ? "fa-volume-high" : "fa-volume-xmark");
+
+  !volume && new Audio("./files/soundOnOff.wav").play();
+  localStorage.setItem("volume", !volume);
+  volume = !volume;
 };
